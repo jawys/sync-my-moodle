f237229d251b0d9517ff3bad8c31e9b2334fde44
jawys <jan.wystub@hsrw.org>
2017-06-14 02:48:13 +0200

    🔨 Added Communication between renderer and main

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const ipc = require('electron').ipcMain
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
@@ -48,3 +49,12 @@ app.on('activate', () => {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+
+ipc.on('save-credentials', (event, credentials) => {
+  console.log('credentials:', credentials)
+})
+
+ipc.on('get-courses', function (event) {
+
+  event.sender.send('received-courses', courses)
+})
