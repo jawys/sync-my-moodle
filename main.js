d723bd2c6ccee9ee5f04586a0ecda66b786344f8
parideis <parideis@users.noreply.github.com>
2017-06-21 19:45:25 +0200

    💄Wider BrowserWindow 💄Added Title and changed body bgc

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ let mainWindow
 
 function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow({width: 600, height: 500})
+  mainWindow = new BrowserWindow({width: 800, height: 600})
 
   // and load the index.html of the app.
   mainWindow.loadURL(path.join('file://', __dirname, 'index.html'))
